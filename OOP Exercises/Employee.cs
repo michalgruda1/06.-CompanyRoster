6971c120dd74ed025a744ffa0fa62b56841b1e3f
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2018-09-10 11:24:14 +0200

    Add project files.

---
+++ OOP Exercises/Employee.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_exercises
+{
+
+    // TODO: Define a class Employee that holds the following information: name, salary, position, department, email and age
+    // TODO: The name, salary, position and department are mandatory while the rest are optional.
+    // TODO: Take N lines of employees from the console
+    // TODO: Calculate the department with the highest average salary
+    // TODO: Print for each employee in that department his name, salary, email and age
+    // TODO: Sort employee by salary in descending order
+    // TODO: If an employee doesn’t have an email – in place of that field you should print “n/a” instead
+    // TODO: if he doesn’t have an age – print “-1” instead
+    // TODO: The salary should be printed to two decimal places after the seperator.
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public float Salary { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string name, string position, float salary, string departament, string email, int age)
+        {
+            Name = name;
+            Position = position;
+            Salary = salary;
+            Department = departament;
+            Email = email;
+            Age = age;
+        }
+
+        public Employee(string name, string position, float salary, string departament) : this(name, position, salary, departament, "n/a", -1) { }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}, department {2}, salary {3:C2}, email {4}, age {5}", Name, Position, Department, Salary, Email, Age);
+        }
+    }
+
+    class Department
+    {
+        public string name;
+        public float avgSalary;
+        private int employeeNumber;
+        private float salarySumOfDept;
+        private Dictionary<string, Employee> departmentEmployees;
+
+        public Department(string depName)
+        {
+            this.name = depName;
+            this.avgSalary = 0F;
+            this.employeeNumber = 0;
+            this.salarySumOfDept = 0F;
+            // this.departmentEmployeeList = new Dictionary<string, float>();
+            this.departmentEmployees = new Dictionary<string, Employee>();
+        }
+
+        /*
+        // this is method that supports old implementation
+        public void AddEmployee(string employeeName, float employeeSalary)
+        {
+            departmentEmployeeList.Add(employeeName, employeeSalary);
+            this.employeeNumber++;
+            this.salarySumOfDept += employeeSalary;
+        }
+        */
+
+        // this method is new implementation
+        public void AddEmployee(string employeeName, Employee employee)
+        {
+            departmentEmployees.Add(employeeName, employee);
+            this.employeeNumber++;
+        }
+        /*
+        // old method
+        public float GetAverage()
+        {
+            return this.avgSalary = salarySumOfDept / (float)employeeNumber;
+        }
+        */
+
+        // new method
+        public float GetAverageSalary()
+        {
+            this.salarySumOfDept = 0F;
+            foreach (Employee employee in departmentEmployees.Values)
+            {
+                this.salarySumOfDept += employee.Salary;
+            }
+            return this.avgSalary = salarySumOfDept / (float)employeeNumber;
+        }
+
+        public Stack<Employee> GetEmployeesSortedBySalary(string departmentName)
+        {
+            SortedList<float, Employee> employeesSortedBySalaryASC = new SortedList<float, Employee>();
+            foreach (Employee emp in departmentEmployees.Values)
+            {
+                if (emp.Department == departmentName)
+                {
+                    employeesSortedBySalaryASC.Add(emp.Salary, emp);
+                }
+            }
+            // we now have a list of employees from selected dept sorted by salary, in ascending order
+            // to fulfill the requirement, we have to sort it by salary in descending order
+            // let's use stack for that
+            Stack<Employee> employeesSortedBySalaryDESC = new Stack<Employee>();
+
+            foreach (Employee emp in employeesSortedBySalaryASC.Values)
+            {
+                employeesSortedBySalaryDESC.Push(emp);
+            }
+            return employeesSortedBySalaryDESC;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder msg = new StringBuilder("\n" + this.name + ", average salary: " + this.GetAverageSalary() + ", no of employees: " + this.employeeNumber);
+            msg.AppendLine("\nHere are the employees:");
+            foreach (KeyValuePair<string, Employee> kvp in departmentEmployees)
+            {
+                msg.AppendFormat("\n{0}, salary: {1:C2}", kvp.Key, kvp.Value.Salary);
+            }
+            return msg.ToString();
+        }
+    }
+}
---
+++ OOP Exercises/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /* test data
+        Michał, Młodszy Programista, 3500, IT
+        Wojtek, Starszy Programista, 10000,IT
+        Sylwia, Starszy UX, 8000,IT
+        Mariusz, UX, 6000,IT
+        Mariusz, Product Manager, 14000, Marketing
+        Paweł, Dyrektor, 18000, Marketing
+        Bartek, PO, 12000, Marketing
+        */
+        {
+            // inform user
+            Console.WriteLine("Write down employees: Name, Position, Salary, Department, email and age:");
+
+            // initialize storage
+            // TODO: Why is this list neccessary?
+            List<Employee> employees = new List<Employee>();
+            Dictionary<string, Department> departments = new Dictionary<string, Department>();
+
+            // do actual user input reading
+            string input = Console.ReadLine();
+
+            // initialize local variables, so compiler does not complain
+            string name = "";
+            string position = "";
+            float salary = 0F;
+            string department = "";
+            string email = "n/a";
+            int age = -1;
+
+            while (input != string.Empty)
+            {
+                string[] separators = { ", ", ",", " , " };
+                string[] line = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                // name, position, department and salary are mandatory
+                // max 6 fields
+
+                if (line.Length < 4)
+                {
+                    throw new Exception("Name, position, salary and department are mandatory");
+                }
+                else if (line.Length == 4)
+                {
+                    name = line[0];
+                    position = line[1];
+                    salary = float.Parse(line[2]);
+                    department = line[3];
+                }
+                else if (line.Length == 5 && line[4].Trim() != "")
+                {
+                    email = line[4];
+                }
+                else if (line.Length == 6 && line[5].Trim() != "")
+                {
+                    bool e = Int32.TryParse(line.ElementAtOrDefault(5), out age);
+                }
+                else
+                {
+                    throw new Exception("Too many fields, maximum 6 fields in line");
+                }
+
+                // add new employee to employee list
+                Employee tempEmployeeObj = new Employee(name, position, salary, department, email, age);
+                employees.Add(tempEmployeeObj);
+
+                // add department of employee
+                try
+                {
+                    // add department to list of depts
+                    departments.Add(department, new Department(department));
+                    Console.WriteLine("Added new department {0}", department);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Department {0} exists", department);
+                }
+
+                // add employee to right dept, and store salary
+                if (departments.ContainsKey(department))
+                {
+                    /* 
+                    // old implementation
+                    departments[department].AddEmployee(name, salary);
+                    */
+
+                    // new implementation
+                    departments[department].AddEmployee(name, tempEmployeeObj);
+                }
+                else
+                {
+                    throw new Exception("Oops, issue with adding a new department " + department);
+                }
+                input = Console.ReadLine();
+            }
+
+            // initialize variables
+            float maxAvgSalary = 0F;
+            string highestPaidDept = "";
+
+            /*
+            // old implementation
+            foreach (KeyValuePair<string,Department> dep in departments)
+            {
+                if (dep.Value.GetAverage() > maxAvgSalary)
+                {
+                    maxAvgSalary = dep.Value.GetAverage();
+                    highestPaidDept = dep.Key;
+                }
+            }
+            Console.WriteLine("Highest paid dept is {0}, avg salary is {1}",highestPaidDept,maxAvgSalary);
+            */
+
+            // new implementation
+            foreach (KeyValuePair<string, Department> kvp in departments)
+            {
+                if (maxAvgSalary < kvp.Value.GetAverageSalary())
+                {
+                    maxAvgSalary = kvp.Value.GetAverageSalary();
+                    highestPaidDept = kvp.Key;
+                }
+            }
+            Console.WriteLine("Highest paid dept is {0}, avg salary is {1:C2}", highestPaidDept, maxAvgSalary);
+
+            Console.WriteLine("Employees of highest paid dept, sorted by salary DESC:");
+
+            foreach (Employee emp in departments[highestPaidDept].GetEmployeesSortedBySalary(highestPaidDept))
+            {
+                Console.WriteLine(emp.ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
